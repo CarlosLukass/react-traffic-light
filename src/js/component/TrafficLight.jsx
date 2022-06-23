f765a72eba6d0f97f4ae03e983d03f31ed888dc2
CarlosLukass <carlos.lukassf@gmail.com>
2022-06-22 22:05:35 -0500

    purple light fixed

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,20 +1,14 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 
 //create your first component
 const TrafficLight = () => {
 
 	// states
-	const [currentLight, setCurrentLight] = useState({
-		color: null,
-		circle: null,
-	});
+	const [currentLight, setCurrentLight] = useState(null);
 	const [isPartyOn, setIsPartyOn] = useState(false)
+	const [purpleIsShowing, setPurpleIsShowing] = useState(false)
 	const [myInterval, setMyInterval] = useState(null)
 
-	// Circle divs refereces
-	const red = useRef(null)
-	const yellow = useRef(null)
-	const green = useRef(null)
 
 	// styles state
 	const styles = {
@@ -28,25 +22,15 @@ const TrafficLight = () => {
 	const handleParty = () => {
 		const colors = ['red', 'yellow', 'green']
 		let colorIndex = 1;
-
-		const divs = {
-			'red': red.current,
-			'yellow': yellow.current,
-			'green': green.current
-		}
+		setPurpleIsShowing(false)
 
 		setIsPartyOn(true)
 
-		setCurrentLight({
-			color: colors[0]
-		})
+		setCurrentLight(colors[0])
 
 		const partyInterval = () =>
 			setInterval(() => {
-				setCurrentLight({
-					color: colors[colorIndex],
-					circle: divs[colors[colorIndex]]
-				})
+				setCurrentLight(colors[colorIndex])
 
 				if (colorIndex === 2) {
 					colorIndex = 0
@@ -61,23 +45,17 @@ const TrafficLight = () => {
 
 	const handleStopParty = () => {
 		clearInterval(myInterval)
-		setCurrentLight({
-			color: '',
-			circle: '',
-		})
+		setCurrentLight('')
 		setIsPartyOn(false)
 	}
 
-	const handlePurple = (e) => {
-		const circle = currentLight.circle;
-		circle.style.backgroundColor = e.target.name
+	const handlePurple = () => {
+		setPurpleIsShowing(true)
+		setCurrentLight('purple')
 	}
 
-	const handleSelector = (e, selector) => {
-		setCurrentLight({
-			color: e.target.name,
-			circle: selector.current
-		})
+	const handleSelector = (e) => {
+		setCurrentLight(e.target.name)
 
 
 	}
@@ -86,21 +64,26 @@ const TrafficLight = () => {
 		<div className="bg-dark vh-100 d-flex flex-column justify-content-center align-items-center">
 			<div className=" d-flex flex-column rounded p-2" style={{ backgroundColor: 'black' }}>
 
-				<div ref={red} className={styles.default} style={currentLight.color === 'red' ? { backgroundColor: 'red' } : {}} />
-				<div ref={yellow} className={styles.default} style={currentLight.color === 'yellow' ? { backgroundColor: 'yellow' } : {}} />
-				<div ref={green} className={styles.default} style={currentLight.color === 'green' ? { backgroundColor: 'green' } : {}} />
+				<div className={styles.default} style={currentLight === 'red' ? { backgroundColor: 'red' } : {}} />
+				<div className={styles.default} style={currentLight === 'yellow' ? { backgroundColor: 'yellow' } : {}} />
+				<div className={styles.default} style={currentLight === 'green' ? { backgroundColor: 'green' } : {}} />
+				{
+					purpleIsShowing
+						? <div className={styles.default} style={currentLight === 'purple' ? { backgroundColor: 'purple' } : {}} />
+						: ''
+				}
 
 			</div>
 
 			<div className="mt-4">
 
-				<button name="red" onClick={e => handleSelector(e, red)} className="btn btn-danger m-2">Red</button>
+				<button name="red" onClick={e => handleSelector(e)} className="btn btn-danger m-2">Red</button>
 
-				<button name="yellow" onClick={e => handleSelector(e, yellow)} className="btn btn-warning m-2">Yellow</button>
+				<button name="yellow" onClick={e => handleSelector(e)} className="btn btn-warning m-2">Yellow</button>
 
-				<button name="green" onClick={e => handleSelector(e, green)} className="btn btn-success m-2">Green</button>
+				<button name="green" onClick={e => handleSelector(e)} className="btn btn-success m-2">Green</button>
 
-				<button name="purple" onClick={e => handlePurple(e)} className="btn btn-outline-light m-2">Purple</button>
+				<button name="purple" onClick={e => handlePurple()} className="btn btn-outline-light m-2">Purple</button>
 			</div>
 			<div>
 				<button className="btn btn-light w-100" onClick={() => isPartyOn ? handleStopParty() : handleParty()}>{isPartyOn ? "Stop" : "Let's Party!!!"}</button>
